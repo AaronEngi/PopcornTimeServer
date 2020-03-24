1dff727660ed985d0023c6b7cfd506d416ab90d7
Tyrael Wang <tyrael.wang@zoom.us>
2020-03-24 14:35:09 +0800

    Initial commit

---
+++ src/main/java/wang/tyrael/PopcornTimeServer/PopcornTimeServerApplication.java
@@ -0,0 +1,13 @@
+package wang.tyrael.PopcornTimeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornTimeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornTimeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wang/tyrael/PopcornTimeServer/PopcornTimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.tyrael.PopcornTimeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornTimeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
